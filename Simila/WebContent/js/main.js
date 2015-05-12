c694ad30f53509fb8250cdac6a0b1d6661841c86
thespeedfreak <bvispi@gmail.com>
2015-05-12 22:01:20 +0200

    added fancybox gallery

--- Simila/WebContent/js/main.js
+++ Simila/WebContent/js/main.js
@@ -1,11 +1,23 @@
-;(function(){
+/*;
+(function() {
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+	// Menu settings
+	$('#menuToggle, .menu-close').on('click', function() {
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
 
+})(jQuery)*/
 
-})(jQuery)
+$(function() {
+	$.stellar({
+		horizontalScrolling : false,
+		verticalOffset : 40
+	});
+});
+
+$(function() {
+	// fancybox
+	jQuery(".fancybox").fancybox();
+});
